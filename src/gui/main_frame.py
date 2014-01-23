e2eaa84f4fb64796ce5776fe19ad7b77480aca9e
xwhuang <xwhuang@qnap.com>
2014-01-23 23:48:01 +0800

    refine gui

--- src/gui/main_frame.py
+++ src/gui/main_frame.py
@@ -4,7 +4,7 @@ try:
 except:
     import Tkinter as tkinter
 
-import ttk
+#import ttk
 
 import core.algorithm
 import core.dup_finder
@@ -25,23 +25,34 @@ class DupFinderWindow(tkinter.Frame):
         for i in range(GUI_INPUT_PATH_NUM):
             self.path_label = tkinter.Label(self)
             self.path_label["text"] = "Path {0}:".format(str(i))
+            if i == 0:
+                self.path_label["text"] += "(Required)"
+            else:
+                self.path_label["text"] += "(Optional)"
             self.path_label.grid(row=i, column=0)
+
+        for i in range(GUI_INPUT_PATH_NUM):
             self.path_field = tkinter.Entry(self)
             self.path_field["width"] = 50
             self.path_field.grid(row=i, column=1, columnspan=6)
             self.path_fields.append(self.path_field)
 
-        self.find_btn = ttk.Button(self)
+        self.find_btn = tkinter.Button(self)
         self.find_btn["text"] = "Find"
         self.find_btn["command"] = self.start_find
         self.find_btn.grid(row=GUI_INPUT_PATH_NUM + 1, column=6)
 
     def start_find(self):
+        LOG.debug("start_find button click")
+        # do some input check
+
+        # start to find
         filters = [
             core.algorithm.SizeFilter(),
             core.algorithm.CharacterFilter()
         ]
         paths = [path_field.get() for path_field in self.path_fields]
+        LOG.debug(paths)
         dup_finder = core.dup_finder.DupFinder(paths, filters)
         dup_finder.find()
         dup_finder.dump2file("output.txt")
