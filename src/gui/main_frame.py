156b31d37e185823b667e1c6862447d950bc2117
xwhuang <xwhuang@qnap.com>
2014-01-24 00:37:28 +0800

    workable first version gui

--- src/gui/main_frame.py
+++ src/gui/main_frame.py
@@ -2,9 +2,11 @@
 try:
     # python 3
     import tkinter
+    from tkinter import filedialog
     from tkinter import messagebox as messagebox
 except ImportError:
     # python 2
+    import tkFileDialog as filedialog
     import Tkinter as tkinter
     import tkMessageBox as messagebox
 
@@ -15,13 +17,13 @@ except ImportError:
     # python 3
     import tkinter.ttk as ttk
 
-
 import core.algorithm
 import core.dup_finder
 from utils import LOG
 
-GUI_INPUT_PATH_NUM = 1
+#GUI_INPUT_PATH_NUM = 1
 
+INITIAL_DIR = "."
 
 class DupFinderWindow(tkinter.Frame):
     def __init__(self, master=None):
@@ -30,37 +32,39 @@ class DupFinderWindow(tkinter.Frame):
         self.createWidgets()
 
     def createWidgets(self):
-        self.path_fields = list()
-        for i in range(GUI_INPUT_PATH_NUM):
-            self.path_label = tkinter.Label(self)
-            self.path_label["text"] = "Path {0}:".format(str(i))
-            if i == 0:
-                self.path_label["text"] += "(Required)"
-            else:
-                self.path_label["text"] += "(Optional)"
-            self.path_label.grid(row=i, column=0)
+        self.path_label = tkinter.Label(self)
+        self.path_label["text"] = "Path(Required):"
+        self.path_label.grid(row=0, column=0)
 
-        for i in range(GUI_INPUT_PATH_NUM):
-            self.path_field = tkinter.Entry(self)
-            self.path_field["width"] = 50
-            self.path_field.grid(row=i, column=1, columnspan=6)
-            self.path_fields.append(self.path_field)
+        self.path_field = tkinter.Label(self)
+        self.path_field["width"] = 50
+        self.path_field["text"] = "."
+        self.path_field.grid(row=0, column=1, columnspan=6)
+
+        self.dir_btn = ttk.Button(self)
+        self.dir_btn["text"] = "Open"
+        self.dir_btn["command"] = self.open_dir
+        self.dir_btn.grid(row=0, column=7)
 
         self.find_btn = ttk.Button(self)
         self.find_btn["text"] = "Find"
         self.find_btn["command"] = self.start_find
-        self.find_btn.grid(row=GUI_INPUT_PATH_NUM + 1, column=6)
+        self.find_btn.grid(row=1, column=7)
+
+    def open_dir(self):
+        path = filedialog.askdirectory(initialdir=INITIAL_DIR)
+        self.path_field.config(text=path)
 
     def start_find(self):
         LOG.debug("start_find button click")
+        LOG.debug(self.path_field.cget("text"))
         # do some input check
-
         # start to find
         filters = [
             core.algorithm.SizeFilter(),
             core.algorithm.CharacterFilter()
         ]
-        paths = [path_field.get() for path_field in self.path_fields]
+        paths = [self.path_field.cget("text")]
         LOG.debug(paths)
         dup_finder = core.dup_finder.DupFinder(paths, filters)
         dup_finder.find()
